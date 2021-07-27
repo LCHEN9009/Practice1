88342388b73d120d294804796b685af94d927829
Louis Chen <88042481+LCHEN9009@users.noreply.github.com>
2021-07-27 14:17:28 +0000

    Created a simple program that asks user for input, and calculate the total.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #This is a practice for learning Python. 
+import turtle
+#problem 2: averages 
+input1 = int(input("Please enter a number: "))
+input2 = int(input("Please enter a number: "))
+input3 = int(input("Please enter a number: "))
+input4 = int(input("Please enter a number: "))
+#four input ask for four numbers
+
+print("This is your average: " + str((input1 + input 2 + input 3 + input 4) / 4))
+#calculates and prints your average
+
+#problem 3: dinner bill
+total = int(input("What is your total bill "))
+#asks user for total bill will input
+dinners = int(input("How many diners ate this meal? "))
+#asks user how any diners were there
+
+print("This is how much each person should pay: " + str(total / diners))
+#calculates and prints how many each person should pay
